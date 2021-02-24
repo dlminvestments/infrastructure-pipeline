68c0f95cba42f5cc0d38a860336585a0148e8913
DLM <55211307+dlminvestments@users.noreply.github.com>
2021-02-24 17:43:28 -0500

    Update main.go

--- main.go
+++ main.go
@@ -21,3 +21,20 @@ import "github.com/accurics/terrascan/pkg/cli"
 func main() {
 	cli.Execute()
 }
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("")
+	fmt.Println("  Congratulations. You started successfully !")
+	fmt.Println("  You'll see, Go is simple and really slick.")
+	fmt.Println("")
+	fmt.Println("  Oh no! a typo -> icanhahz")
+	fmt.Println("")
+	fmt.Println("  Correct it and re-run this program.")
+	fmt.Println("")
+	fmt.Println("  You can modify this program by tweaking `main.go`")
+	fmt.Println("")
+}
